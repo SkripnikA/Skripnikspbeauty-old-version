a8a1c1016561c252617ff9ab666c18cebbe9efb7
SkripnikA <123431340+SkripnikA@users.noreply.github.com>
2023-01-28 17:50:49 +0000

    ховеры

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -5,17 +5,10 @@ function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
+        border: 1px solid #64748B;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: #152842;
-        &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
-        }
-        &:active {
-            background: #DFDFDF;
-        }
     `;
     return (
         <Clickable >
